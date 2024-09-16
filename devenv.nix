1306d752540fe56d9c7fe6bd3ec927b57619087e
Martín Pozo <mpozo@protonmail.com>
2024-09-16 17:20:39 +0200

    [cegar_disambiguation] Devenv config files

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  enterShell = ''
+    export PATH="$PATH:$HOME/workspace/uncrustify/uncrustify-uncrustify-0.72.0/build/bin"
+  '';
+
+  languages.python = {
+    enable = true;
+    poetry.enable = false;
+    uv.enable = false;
+    venv.enable = true;
+  };
+
+  packages = with pkgs; [
+    ruff
+    cmake
+    clang-tools
+    valgrind
+  ];
+}
