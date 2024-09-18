19c56dbc428d17ab14ad560062c99216b28b40fd
Martín Pozo <mpozo@protonmail.com>
2024-09-18 15:26:11 +0200

    [flaw_selection_strategies] SoPlex in devenv config files

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,11 @@
 { pkgs, lib, config, inputs, ... }:
-
+let
+  pkgs-unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
+in
 {
   enterShell = ''
     export PATH="$PATH:$HOME/workspace/uncrustify/uncrustify-uncrustify-0.72.0/build/bin"
+    export soplex_DIR="/nix/store/gpih1cs4dypyv5q31ny1avb9b6r1mrnw-soplex-7.1.0"
   '';
 
   languages.python = {
@@ -17,5 +20,7 @@
     cmake
     clang-tools
     valgrind
+    pkgs-unstable.gmp
+    pkgs-unstable.soplex
   ];
 }
