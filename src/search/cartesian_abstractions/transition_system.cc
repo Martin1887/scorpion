1b5419eb808acd31a279767fdf0eecd2456180c4
Martín Pozo <mpozo@protonmail.com>
2024-12-04 10:14:41 +0100

    [conditional_effects] Fix code style error (init map in constructor)

--- src/search/cartesian_abstractions/transition_system.cc
+++ src/search/cartesian_abstractions/transition_system.cc
@@ -110,8 +110,7 @@ static void remove_transitions_with_given_target(
 
 
 TransitionSystem::TransitionSystem(const OperatorsProxy &ops)
-    : cond_effects_by_op_id(),
-      preconditions_by_operator(get_preconditions_by_operator(ops)),
+    : preconditions_by_operator(get_preconditions_by_operator(ops)),
       postconditions_by_operator(get_postconditions_by_operator(ops, cond_effects_by_op_id)),
       num_non_loops(0),
       num_loops(0) {
--- src/search/cartesian_abstractions/transition_system.h
+++ src/search/cartesian_abstractions/transition_system.h
@@ -40,7 +40,7 @@ enum AddTransitionToChild {
   Rewire transitions after each split.
 */
 class TransitionSystem {
-    std::unordered_map<int, std::vector<CondEffect>> cond_effects_by_op_id;
+    std::unordered_map<int, std::vector<CondEffect>> cond_effects_by_op_id = {};
     const std::vector<std::vector<FactPair>> preconditions_by_operator;
     const std::vector<std::vector<FactPair>> postconditions_by_operator;
     // Vector used to store post values for each child for ops with conditional
