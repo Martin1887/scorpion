d7559c6b85f27007005732288755a7d993f6c272
Martín Pozo <mpozo@protonmail.com>
2026-01-11 10:39:35 +0100

    [conditional_effects] Fix that for non-domain-abstracted landmark subtasks unreachable before goals were not copmuted, being non-admissible

--- src/search/cartesian_abstractions/cegar.cc
+++ src/search/cartesian_abstractions/cegar.cc
@@ -74,8 +74,7 @@ CEGAR::CEGAR(
         log << "Maximum time: " << timer.get_remaining_time() << endl;
     }
 
-    bool is_landmark_subtask = dynamic_cast<extra_tasks::DomainAbstractedTask *>(task.get());
-    refinement_loop(is_landmark_subtask);
+    refinement_loop();
     if (log.is_at_least_normal()) {
         log << "Done building abstraction." << endl;
         log << "Time for building abstraction: " << timer.get_elapsed_time() << endl;
@@ -164,7 +163,7 @@ bool CEGAR::may_keep_refining() const {
     return true;
 }
 
-void CEGAR::refinement_loop(bool is_landmark_subtask) {
+void CEGAR::refinement_loop() {
     /*
       For landmark tasks we have to map all states in which the
       landmark might have been achieved to arbitrary abstract goal
@@ -174,7 +173,7 @@ void CEGAR::refinement_loop(bool is_landmark_subtask) {
       to simplify the implementation. This way, we don't have to split
       goal states later.
     */
-    if (is_landmark_subtask) {
+    if (task_proxy.get_goals().size() == 1) {
         separate_facts_unreachable_before_goal();
     } else {
         // Iteratively split off the next goal fact from the current goal state.
